6f8e1c68da21591690634c2cbe67bb46d056aaa1
akishtp <makishtp@gmail.com>
2023-01-11 20:09:51 +0530

    seperate records for account should work

--- routes/account.js
+++ routes/account.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const {
+  getAccounts,
+  addAccount,
+  deleteAccount,
+  updateAccount,
+} = require("../controllers/accountControllers");
 const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
 router.use(requireAuth);
 
-router.get("/", () => {});
-router.post("/", () => {});
-router.delete("/:id", () => {});
-router.put("/:id", () => {});
+router.get("/", getAccounts);
+router.post("/", addAccount);
+router.delete("/:id", deleteAccount);
+router.put("/:id", updateAccount);
 
 module.exports = router;
